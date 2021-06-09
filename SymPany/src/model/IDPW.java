dac55867aa01ca60c8a0fdff136c52696a456ea2
h-saigo <saigo@seplus2016.onmicrosoft.com>
2021-06-09 17:10:40 +0900

    commit

---
+++ SymPany/src/model/IDPW.java
@@ -0,0 +1,32 @@
+package model;
+
+public class IDPW {
+ private String user_id;
+ private String password;
+
+ public IDPW(String user_id,String password) {
+	 this.user_id = user_id;
+	 this.password = password;
+ }
+
+ public IDPW(){
+	 this.user_id="";
+	 this.password="";
+ }
+
+public String getUser_id() {
+	return user_id;
+}
+public void setUser_id(String user_id) {
+	this.user_id = user_id;
+}
+
+public String getPassword() {
+	return password;
+}
+public void setPassword(String password) {
+	this.password = password;
+}
+
+
+}
