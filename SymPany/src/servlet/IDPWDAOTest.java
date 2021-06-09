6b07a1883b893d274f1bc066746be3a1addb80ea
h-saigo <saigo@seplus2016.onmicrosoft.com>
2021-06-09 17:47:11 +0900

    テストファイルもコミットしてあります。

---
+++ SymPany/src/servlet/IDPWDAOTest.java
@@ -0,0 +1,31 @@
+package servlet;
+
+import DAO.IDPWDAO;
+public class IDPWDAOTest {
+	public static void main(String[] args) {
+		testIsLoginOK1();	// ユーザーが見つかる場合のテスト
+		testIsLoginOK2();	// ユーザーが見つからない場合のテスト
+	}
+
+	// ユーザーが見つかる場合のテスト
+	public static void testIsLoginOK1() {
+		IDPWDAO dao = new IDPWDAO();
+		if (dao.isLoginOK("DOJO", "password")) {
+			System.out.println("testIsLoginOK1：テストが成功しました");
+		}
+		else {
+			System.out.println("testIsLoginOK1：テストが失敗しました");
+		}
+	}
+
+	// ユーザーが見つからない場合のテスト
+	public static void testIsLoginOK2() {
+		IDPWDAO dao = new IDPWDAO();
+		if (!dao.isLoginOK("DOJO", "pass")) {
+			System.out.println("testIsLoginOK2：テストが成功しました");
+		}
+		else {
+			System.out.println("testIsLoginOK2：テストが失敗しました");
+		}
+	}
+}
